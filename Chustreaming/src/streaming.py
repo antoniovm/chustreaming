f0ea1bfc6a5420d09a25509fc869bee49154d615
loopin.loopin <loopin.loopin@gmail.com>
2012-03-06 09:08:55 +0000

    Ruta dropbox

--- Chustreaming/src/streaming.py
+++ Chustreaming/src/streaming.py
@@ -5,11 +5,11 @@ Created on 06/03/2012
 '''
 import httplib
 conn = httplib.HTTPConnection("localhost:8000")
-conn.request("GET", "/emitiendo.ogg")
+conn.request("GET", "/lista.ogg")
 r1 = conn.getresponse()
 print r1.status, r1.reason
 data1 = r1.read()
-f = open('/home/jorge/Documentos/canciones/streamPython.ogg', 'w')
+f = open('/mnt/hgfs/Chustafile/Tercer curso/Segundo cuatrimestre/Redes/Streaming/recepcion/tangerineDream.ogg', 'w')
 print >> f, data1
 print data1
 conn.close()
